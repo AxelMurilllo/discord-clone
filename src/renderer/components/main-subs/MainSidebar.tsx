444cd6b237eeb71402ae42f1cef767e125a15739
AxelMurilllo <axelmurillo@u.boisestate.edu>
2024-07-29 23:59:43 -0600

    Updated Server routing

--- src/renderer/components/main-subs/MainSidebar.tsx
+++ src/renderer/components/main-subs/MainSidebar.tsx
@@ -1,10 +1,17 @@
 import { MDBCol } from "mdb-react-ui-kit";
 import React from "react";
+import ServerBtn from "../utils/ServerBtns";
 
 const MainSidebar = React.memo((props: any) => {
     return (
-        <MDBCol md={3} className = "main-sidebar bg-info">
-            
+        <MDBCol md={3} className = "main-sidebar flex-wrap d-flex bg-info">
+            <div className="w-100 bg-danger" style={{height: 10, flexShrink: 0}}></div>
+            <ServerBtn 
+            title='Test Server' 
+            url="server/0"
+            img="https://imageio.forbes.com/specials-images/imageserve/5f962df3991e5636a2f68758/0x0.jpg"
+            />
+
         </MDBCol>
     )
 })
